b8826cd0fbf779130a439c447545dbdd10328779
Diego Maia <diegosantosmaia@gmail.com>
2016-11-09 10:40:41 -0200

    Ver 1.0

---
+++ README.md
@@ -0,0 +1,16 @@
+# Switch Datacom-backup
+Backup da configuração do switch Datacom
+
+Editar o arquivo swdatacom-backup.sh e mudar o usuário e senha
+
+Exceutar:
+
+./swdatacom-backup.sh ipdoequipamento nomedoarquivo
+
+
+Para deixar automatico você coloca no contrab para executar 1x por dia ou 1x por semana
+depende da quantidade de vezes que você muda a configuração, assim sempre o backup vai
+estar atualizado.
+
+
+https://br.linkedin.com/pub/diego-santos-maia/2b/b60/173
---
+++ swdatacom-backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+##########################################################################
+# Backup-Switch-Datacom
+# Filename: swdatacom-backup.sh
+# Revision: 1.0
+# Date: 09/11/2016
+# Author: Diego Maia - diegosmaia@yahoo.com.br Telegram - @diegosmaia
+# Executar: 
+# ./swdatacom-backup.sh 192.168.0.1 swdatacom-empresa
+##########################################################################
+
+###################
+#Dados fornecidos pelo usuário
+###################
+IP=$1
+FILENAME=$2
+
+######################
+# Usuario do Sw Datacom
+######################
+username='dmaia'
+password='dmaiasenha'
+
+###################################
+# Logando no telnet do equipamento
+###################################
+
+{ echo $username; sleep 1;echo $password; sleep 1;echo "show running-config"; sleep 1; echo "close"; echo "exit" } | telnet $IP > $FILENAME
+
